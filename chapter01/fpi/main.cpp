5eaf438acdfa5f0dcbd045af58f8f311c9e4c2a7
Yangtzhou <yangtzhou@qq.com>
2016-08-11 21:54:05 +0800

    Update fixed point iteration algorithm.

--- chapter01/fpi/main.cpp
+++ chapter01/fpi/main.cpp
@@ -15,14 +15,14 @@ double fpi(double (*f)(double), double x0, double loops)
 
 	for(i = 0; i < loops; ++i) {
 #ifdef _FPI_DEBUG_
-		cout << fixed << setw(11) << setprecision(8) << setiosflags(ios::showpos);
+		cout << fixed << setw(12) << setprecision(9) << setiosflags(ios::showpos);
 		cout << i << '\t' << x0 << endl;
 #endif
 		x0 = f(x0);
 	}
 
 #ifdef _FPI_DEBUG_
-	cout << fixed << setw(11) << setprecision(8) << setiosflags(ios::showpos);
+	cout << fixed << setw(12) << setprecision(9) << setiosflags(ios::showpos);
 	cout << i << '\t' << x0 << endl;
 #endif
 
@@ -44,6 +44,21 @@ double f3(double x)
 	return (1 + 2*x*x*x) / (1 + 3*x*x);
 }
 
+double f4(double x)
+{
+	return x + cos(x) - sin(x);
+}
+
+double f5(double x)
+{
+	return 2.8*x - x*x;
+}
+
+double f6(double x)
+{
+	return (x + 2/x) / 2;
+}
+
 int main()
 {
 	double x1 = fpi(f1, 0.5, 12);
@@ -54,6 +69,15 @@ int main()
 
 	double x3 = fpi(f3, 0.5, 7);
 	cout << x3 << endl << endl;
+
+	double x4 = fpi(f4, 0, 20);
+	cout << x4 << endl << endl;
+
+	double x5 = fpi(f5, 0.1, 10);
+	cout << x5 << endl << endl;
+
+	double x6 = fpi(f6, 1, 20);
+	cout << x6 << endl << endl;
 
 	return 0;
 }
